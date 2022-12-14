194b8e5e29a95690569045f40de8d2a1a09da8b5
Jan Niklas Richter <git@janniklasrichter.de>
2022-12-14 22:44:10 +0000

    Refactor day12

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,8 +1,7 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, VecDeque};
 
 use bitvec::macros::internal::funty::Fundamental;
 use itertools::Itertools;
-use priority_queue::PriorityQueue;
 
 type Coord2D = (isize, isize);
 
@@ -20,44 +19,36 @@ fn add(coord: Coord2D, other: Coord2D) -> Coord2D {
     (coord.0 + other.0, coord.1 + other.1)
 }
 
-fn distance(one: Coord2D, other: Coord2D) -> usize {
-    // Manhatten distance for now
-    ((one.0 - other.0).abs() + (one.1 - other.1).abs()) as usize
-}
-
-fn pathfinding(grid: &Vec<Vec<u32>>, start: Coord2D, end: Coord2D) -> Vec<Coord2D> {
-    let mut closed_nodes: HashSet<Coord2D> = HashSet::new();
-    let mut solution: HashMap<Coord2D, (Coord2D, u32)> = HashMap::new();
-    let mut queue: PriorityQueue<(Coord2D, u32), usize> = PriorityQueue::new();
-    let max_distance = grid.len() * grid[0].len();
-
-    queue.push((start, 0), max_distance - distance(start, end));
+fn pathfinding(
+    grid: &Vec<Vec<u32>>,
+    start: Coord2D,
+    f: impl Fn(Coord2D, u32) -> bool,
+) -> Vec<Coord2D> {
+    let mut solution: HashMap<Coord2D, Coord2D> = HashMap::new();
+    let mut queue: VecDeque<Coord2D> = VecDeque::from([start]);
+    let mut end: Coord2D = (0, 0);
 
     while !queue.is_empty() {
-        let current_node = queue.pop();
+        let current_node = queue.pop_front();
 
-        if let Some(((pos, current_pathlength), _)) = current_node {
-            closed_nodes.insert(pos);
+        if let Some(pos) = current_node {
+            if f(pos, grid[pos.1 as usize][pos.0 as usize]) {
+                end = pos;
+                break;
+            }
 
             for n in neighbors(pos, grid) {
-                if closed_nodes.contains(&n) {
+                if solution.contains_key(&n) {
                     continue;
                 }
-                if grid[n.1 as usize][n.0 as usize] > grid[pos.1 as usize][pos.0 as usize] + 1 {
+                if (grid[n.1 as usize][n.0 as usize] as i32)
+                    < ((grid[pos.1 as usize][pos.0 as usize] as i32) - 1)
+                {
                     continue;
                 }
 
-                queue.push(
-                    (n, current_pathlength + 1),
-                    max_distance - (distance(n, end) + current_pathlength as usize),
-                );
-                if let Some((_, old_pathlength)) = solution.get(&n) {
-                    if *old_pathlength > current_pathlength {
-                        solution.insert(n, (pos, current_pathlength + 1));
-                    }
-                } else {
-                    solution.insert(n, (pos, current_pathlength + 1));
-                }
+                queue.push_back(n);
+                solution.insert(n, pos);
             }
         }
     }
@@ -66,7 +57,7 @@ fn pathfinding(grid: &Vec<Vec<u32>>, start: Coord2D, end: Coord2D) -> Vec<Coord2
     let mut path = vec![current];
     while current != start {
         let solution_item = solution.get(&current).unwrap();
-        current = solution_item.0;
+        current = *solution_item;
         path.push(current);
     }
     path.reverse();
@@ -112,26 +103,6 @@ fn parse_input(input: &str) -> (Vec<Vec<u32>>, Coord2D, Coord2D) {
     (grid, start, end)
 }
 
-fn get_all_possible_starting_locations(grid: &Vec<Vec<u32>>) -> Vec<Coord2D> {
-    // just take all a locations that have a b location adjacent
-    let mut locations = vec![];
-    for (y, row) in grid.iter().enumerate() {
-        for (x, col) in row.iter().enumerate() {
-            if *col != 0 {
-                continue;
-            }
-
-            if neighbors((x as isize, y as isize), grid)
-                .iter()
-                .any(|(nx, ny)| grid[*ny as usize][*nx as usize] == 1)
-            {
-                locations.push((x as isize, y as isize));
-            }
-        }
-    }
-    locations
-}
-
 fn print_grid(grid: &[Vec<u32>], highlight: &[Coord2D]) {
     for (y, row) in grid.iter().enumerate() {
         for (x, col) in row.iter().enumerate() {
@@ -147,7 +118,7 @@ fn print_grid(grid: &[Vec<u32>], highlight: &[Coord2D]) {
 
 pub fn part_one(_input: &str) -> Option<u32> {
     let (grid, start, end) = parse_input(_input);
-    let path = pathfinding(&grid, start, end);
+    let path = pathfinding(&grid, end, |pos, _| pos == start);
     print_grid(&grid, &path);
 
     Some(path.len() as u32 - 1)
@@ -156,17 +127,11 @@ pub fn part_one(_input: &str) -> Option<u32> {
 pub fn part_two(_input: &str) -> Option<u32> {
     // brute force over all a locations adjacent to b locations
     let (grid, _, end) = parse_input(_input);
-    let possible_starts = get_all_possible_starting_locations(&grid);
-    let mut best_path = vec![];
-    for (i, s) in possible_starts.iter().enumerate() {
-        let path = pathfinding(&grid, *s, end);
-        if i == 0 || path.len() < best_path.len() {
-            best_path = path;
-        }
-    }
-    print_grid(&grid, &best_path);
+    let path = pathfinding(&grid, end, |_, elevation| elevation == 0);
 
-    Some(best_path.len() as u32 - 1)
+    print_grid(&grid, &path);
+
+    Some(path.len() as u32 - 1)
 }
 
 fn main() {
