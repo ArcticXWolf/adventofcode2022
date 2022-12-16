02a7374ba3b0ddccaf978281d02788e76f730301
Jan Niklas Richter <git@janniklasrichter.de>
2022-12-16 18:12:24 +0000

    Refactor day16

--- src/bin/16.rs
+++ src/bin/16.rs
@@ -1,12 +1,10 @@
-use std::collections::{HashMap, HashSet, VecDeque};
-
 use itertools::Itertools;
+use ndarray::Array3;
 use regex::Regex;
 
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
 struct Node {
     name: String,
-    id: usize,
     flow: u32,
     neighbor_strings: Vec<String>,
     neighbors: Vec<usize>,
@@ -19,24 +17,29 @@ fn parse_input_into_nodes(input: &str) -> (Vec<Node>, usize) {
     let mut result = vec![];
     let mut start_id = 0;
 
-    for (i, line) in input.lines().enumerate() {
+    for line in input.lines() {
         let re_match = re.captures(line.trim()).unwrap();
         result.push(Node {
             name: re_match[1].to_string(),
-            id: i,
             flow: re_match[2].parse().unwrap(),
             neighbor_strings: re_match[3].split(", ").map(|x| x.to_string()).collect(),
             neighbors: vec![],
         });
-        if &re_match[1] == "AA" {
-            start_id = i;
-        }
     }
 
+    result.sort_by(|a, b| b.flow.cmp(&a.flow));
+
     for i in 0..result.len() {
+        if result[i].name == "AA" {
+            start_id = i;
+        }
         for j in 0..result[i].neighbor_strings.len() {
             let neighbor_name = &result[i].neighbor_strings[j];
-            let neighbor_node = result.iter().find(|n| n.name == *neighbor_name).unwrap().id;
+            let neighbor_node = result
+                .iter()
+                .find_position(|n| n.name == *neighbor_name)
+                .unwrap()
+                .0;
             result[i].neighbors.push(neighbor_node);
         }
     }
@@ -44,210 +47,108 @@ fn parse_input_into_nodes(input: &str) -> (Vec<Node>, usize) {
     (result, start_id)
 }
 
-fn print_dot_format(graph: &[Node]) {
-    let mut closed = HashSet::new();
-    println!("graph valves {{");
-    for n in graph.iter() {
-        println!("{} [label=\"{} - {}\"]", n.name, n.name, n.flow);
-    }
+// Dynamic programming approach used from
+// https://www.reddit.com/r/adventofcode/comments/zn6k1l/2022_day_16_solutions/
+// My initial original solution can be found in the git history, but part 2 took
+// 2min runtime (because of brute force depth search) and thus was too long for
+// my complete AOC runtime. So if you are interested in my initial solution
+// scroll back in commit history.
+fn find_maximum_pressure_path_dp(
+    graph: &[Node],
+    max_time_left: u32,
+) -> ndarray::ArrayBase<ndarray::OwnedRepr<u32>, ndarray::Dim<[usize; 3]>> {
+    let amount_of_relevant_valves = graph.iter().filter(|n| n.flow > 0).count();
+    let amount_of_relevant_valve_configurations: u32 = 1 << amount_of_relevant_valves;
 
-    for n in graph.iter() {
-        closed.insert(n.name.clone());
-        for nn in &n.neighbor_strings {
-            if closed.contains(nn) {
-                continue;
+    // ndarrays are so much faster than HashMap/Array, etc
+    let mut solution = Array3::<u32>::zeros((
+        max_time_left as usize,
+        graph.len(),
+        amount_of_relevant_valve_configurations as usize,
+    ));
+
+    // Iterate over all possible states
+    // States are: amount of time left, position in graph, opened valves
+    // Valve configuration is saved as bitmask with 0 = open and 1 = closed
+    // remember we move backwards in time (start from last timeslot), so we can
+    // only refer to future states
+    for time_left in 1..max_time_left {
+        for current_node in 0..graph.len() {
+            let current_node_valve_bit = 1 << current_node;
+            for valve_configuration in 0..amount_of_relevant_valve_configurations {
+                // Calculate result from surrounding states (future states!)
+                let mut result = 0;
+
+                if current_node_valve_bit & valve_configuration != 0 && time_left > 1 {
+                    // Check valve opening transition
+                    // -> we are at valve closed, so the step later is open
+                    // -> take step later result and add flow of our valve until end of time
+                    result = solution[(
+                        (time_left - 1) as usize,
+                        current_node,
+                        (valve_configuration - current_node_valve_bit) as usize,
+                    )] + graph[current_node].flow * time_left;
+                }
+
+                // result now might contain our best flow if we open the valve
+                // at current position now check if it is better to not open and
+                // move to another position (same configuration)
+                for neighbor in graph[current_node].neighbors.iter() {
+                    result = result.max(
+                        solution[(
+                            (time_left - 1) as usize,
+                            *neighbor,
+                            valve_configuration as usize,
+                        )],
+                    )
+                }
+
+                // save
+                solution[(
+                    time_left as usize,
+                    current_node,
+                    valve_configuration as usize,
+                )] = result;
             }
-            println!("  {} -- {};", n.name, nn);
         }
     }
-    println!("}}");
-}
 
-fn get_all_paths(graph: &[Node]) -> HashMap<(usize, usize), u32> {
-    let mut result = HashMap::new();
-    for node in graph.iter() {
-        let solutions = bfs(graph, node.id);
-        for (target, distance) in solutions {
-            result.insert((node.id, target), distance);
-        }
-    }
-    result
-}
-
-fn bfs(graph: &[Node], start: usize) -> HashMap<usize, u32> {
-    let mut closed = HashSet::new();
-    let mut solution = HashMap::new();
-    let mut queue = VecDeque::new();
-    queue.push_back((start, 0));
-
-    while let Some((current_id, current_distance)) = queue.pop_front() {
-        let current_node = graph.iter().find(|n| n.id == current_id).unwrap();
-
-        for neighbor_id in current_node.neighbors.iter() {
-            if closed.contains(&neighbor_id) {
-                continue;
-            }
-
-            queue.push_back((*neighbor_id, current_distance + 1));
-            closed.insert(neighbor_id);
-
-            let neighbor = graph.iter().find(|n| n.id == *neighbor_id).unwrap();
-            if neighbor.flow > 0 {
-                solution.insert(*neighbor_id, current_distance + 1);
-            }
-        }
-    }
     solution
-}
-
-fn find_maximum_pressure_path(
-    graph: &[Node],
-    precomputed_paths: &HashMap<(usize, usize), u32>,
-    current_node_id: usize,
-    current_time: u32,
-    current_flow: u32,
-    closed: &[usize],
-) -> (Vec<usize>, u32) {
-    if current_time >= 30 {
-        return (vec![], current_flow);
-    }
-
-    let nodes_possible = precomputed_paths
-        .keys()
-        .filter(|(s, t)| *s == current_node_id && !closed.contains(t));
-
-    let mut best_path = vec![];
-    let mut best_flow = current_flow;
-    for (_, target) in nodes_possible {
-        let time_to_move_to_target_and_activate =
-            precomputed_paths[&(current_node_id, *target)] + 1;
-        if current_time + time_to_move_to_target_and_activate >= 30 {
-            continue;
-        }
-
-        let target_flow = graph.iter().find(|n| n.id == *target).unwrap().flow
-            * (30 - current_time - time_to_move_to_target_and_activate);
-        let mut new_closed = closed.iter().cloned().collect_vec();
-        new_closed.push(*target);
-
-        let (path, flow) = find_maximum_pressure_path(
-            graph,
-            precomputed_paths,
-            *target,
-            current_time + time_to_move_to_target_and_activate,
-            current_flow + target_flow,
-            &new_closed,
-        );
-
-        if flow > best_flow {
-            best_path = path;
-            best_flow = flow;
-        }
-    }
-
-    best_path.push(current_node_id);
-    (best_path, best_flow)
-}
-
-fn find_maximum_pressure_path_with_elephant(
-    graph: &[Node],
-    precomputed_paths: &HashMap<(usize, usize), u32>,
-    current_node_ids: (usize, usize),
-    current_times: (u32, u32),
-    current_flow: u32,
-    closed: &[usize],
-) -> (Vec<(usize, usize)>, u32) {
-    if current_times.0 >= 26 && current_times.1 >= 26 {
-        return (vec![], current_flow);
-    }
-
-    let (current_node_id, current_time, is_me) = if current_times.0 < current_times.1 {
-        (current_node_ids.0, current_times.0, true)
-    } else {
-        (current_node_ids.1, current_times.1, false)
-    };
-
-    let nodes_possible = precomputed_paths
-        .keys()
-        .filter(|(s, t)| *s == current_node_id && !closed.contains(t));
-
-    let mut best_path: Vec<(usize, usize)> = vec![];
-    let mut best_flow = current_flow;
-
-    for (_, target) in nodes_possible {
-        let time_to_move_to_target_and_activate =
-            precomputed_paths[&(current_node_id, *target)] + 1;
-
-        if current_time + time_to_move_to_target_and_activate >= 26 {
-            continue;
-        }
-
-        let target_flow = graph.iter().find(|n| n.id == *target).unwrap().flow
-            * (26 - current_time - time_to_move_to_target_and_activate);
-        let (next_ids, next_times) = if is_me {
-            (
-                (*target, current_node_ids.1),
-                (
-                    current_time + time_to_move_to_target_and_activate,
-                    current_times.1,
-                ),
-            )
-        } else {
-            (
-                (current_node_ids.0, *target),
-                (
-                    current_times.0,
-                    current_time + time_to_move_to_target_and_activate,
-                ),
-            )
-        };
-        let mut new_closed = closed.iter().cloned().collect_vec();
-        new_closed.push(*target);
-
-        let (path, flow) = find_maximum_pressure_path_with_elephant(
-            graph,
-            precomputed_paths,
-            next_ids,
-            next_times,
-            current_flow + target_flow,
-            &new_closed,
-        );
-
-        if flow > best_flow {
-            best_path = path;
-            best_flow = flow;
-        }
-    }
-
-    best_path.push(current_node_ids);
-    (best_path, best_flow)
 }
 
 pub fn part_one(_input: &str) -> Option<u32> {
     let (graph, start) = parse_input_into_nodes(_input);
-    let paths = get_all_paths(&graph);
+    let amount_of_relevant_valves = graph.iter().filter(|n| n.flow > 0).count();
+    let amount_of_relevant_valve_configurations = 1 << amount_of_relevant_valves;
 
-    let (mut solution, flow) = find_maximum_pressure_path(&graph, &paths, start, 0, 0, &[]);
-    solution.reverse();
+    let solution = find_maximum_pressure_path_dp(&graph, 30);
 
-    // print_dot_format(&graph);
-    // println!("{:?}", solution);
-
-    Some(flow)
+    Some(solution[(29, start, amount_of_relevant_valve_configurations - 1)])
 }
 
 pub fn part_two(_input: &str) -> Option<u32> {
     let (graph, start) = parse_input_into_nodes(_input);
-    let paths = get_all_paths(&graph);
+    let amount_of_relevant_valves = graph.iter().filter(|n| n.flow > 0).count();
+    let amount_of_relevant_valve_configurations = 1 << amount_of_relevant_valves;
+    let mut tracker = 0;
 
-    let (mut solution, flow) =
-        find_maximum_pressure_path_with_elephant(&graph, &paths, (start, start), (0, 0), 0, &[]);
-    solution.reverse();
+    let solution = find_maximum_pressure_path_dp(&graph, 30);
 
-    // print_dot_format(&graph);
-    println!("{:?}", solution);
+    // Give me and the elephant different valves to work with.  Those
+    // assignments must be disjunct. Also it is okay to only check total
+    // assignments (no valves not assigned) because the dynamic programming
+    // table accounts for not using valves
+    for me in 0..amount_of_relevant_valve_configurations {
+        for elephant in 0..amount_of_relevant_valve_configurations {
+            if me & elephant != 0 {
+                continue;
+            }
+            let flow = solution[(25, start, me)] + solution[(25, start, elephant)];
+            tracker = tracker.max(flow);
+        }
+    }
 
-    Some(flow)
+    Some(tracker)
 }
 
 fn main() {
