8a988b5d51dee24d7ddab5cf3d294d32c6f60efd
Jan Niklas Richter <git@janniklasrichter.de>
2022-12-18 17:43:13 +0000

    Solve day18

--- src/bin/18.rs
+++ src/bin/18.rs
@@ -4,7 +4,7 @@ use std::iter::FromIterator;
 use advent_of_code::helpers::Point3;
 use itertools::Itertools;
 
-fn parse_input(input: &str) -> HashSet<Point3<i32>> {
+fn parse_input(input: &str) -> (HashSet<Point3<i32>>, Point3<i32>, Point3<i32>) {
     let points = input.trim().lines().map(|l| {
         let split = l.split(',').collect_tuple::<(&str, &str, &str)>().unwrap();
         Point3 {
@@ -13,51 +13,95 @@ fn parse_input(input: &str) -> HashSet<Point3<i32>> {
             z: split.2.parse::<i32>().unwrap(),
         }
     });
-    HashSet::from_iter(points)
+    let minpoint = Point3 {
+        x: points.clone().map(|p| p.x).min().unwrap(),
+        y: points.clone().map(|p| p.y).min().unwrap(),
+        z: points.clone().map(|p| p.z).min().unwrap(),
+    };
+    let maxpoint = Point3 {
+        x: points.clone().map(|p| p.x).max().unwrap(),
+        y: points.clone().map(|p| p.y).max().unwrap(),
+        z: points.clone().map(|p| p.z).max().unwrap(),
+    };
+    (HashSet::from_iter(points), minpoint, maxpoint)
 }
 
-fn count_open_sides(points: HashSet<Point3<i32>>) -> u32 {
-    let mut closed: HashSet<Point3<i32>> = HashSet::with_capacity(points.len());
-    let mut queue: VecDeque<Point3<i32>> = VecDeque::new();
+fn count_open_sides(points: &HashSet<Point3<i32>>) -> u32 {
     let mut tracker = 0;
 
-    while points.intersection(&closed).count() != points.len() {
-        queue.push_back(points.difference(&closed).next().cloned().unwrap());
-        while !queue.is_empty() {
-            if let Some(point) = queue.pop_front() {
-                // print!("Cube: {:?}", point);
-                let tracker_old = tracker;
-                closed.insert(point);
+    for point in points.iter() {
+        for d in Point3::directions() {
+            let neighbor = *point + d;
 
-                for d in Point3::directions() {
-                    let neighbor = point + d;
-
-                    if closed.contains(&neighbor) {
-                        continue;
-                    }
-
-                    if points.contains(&neighbor) {
-                        queue.push_back(neighbor);
-                        continue;
-                    }
-
-                    tracker += 1;
-                }
-                // println!(", open {}", tracker - tracker_old);
+            if points.contains(&neighbor) {
+                continue;
             }
+
+            tracker += 1;
         }
     }
 
     tracker
 }
 
+fn find_connected_points(
+    points: &HashSet<Point3<i32>>,
+    start: &Point3<i32>,
+) -> HashSet<Point3<i32>> {
+    let mut closed: HashSet<Point3<i32>> = HashSet::new();
+    let mut queue: VecDeque<Point3<i32>> = VecDeque::new();
+
+    queue.push_back(*start);
+    while !queue.is_empty() {
+        if let Some(point) = queue.pop_front() {
+            for d in Point3::directions() {
+                let neighbor = point + d;
+
+                if closed.contains(&neighbor) {
+                    continue;
+                }
+
+                if points.contains(&neighbor) {
+                    queue.push_back(neighbor);
+                    closed.insert(neighbor);
+                    continue;
+                }
+            }
+        }
+    }
+
+    closed
+}
+
+fn create_cube(minpoint: &Point3<i32>, maxpoint: &Point3<i32>) -> HashSet<Point3<i32>> {
+    let mut closed: HashSet<Point3<i32>> = HashSet::new();
+    for x in minpoint.x..maxpoint.x {
+        for y in minpoint.y..maxpoint.y {
+            for z in minpoint.z..maxpoint.z {
+                closed.insert(Point3 { x, y, z });
+            }
+        }
+    }
+    closed
+}
+
 pub fn part_one(_input: &str) -> Option<u32> {
-    let points = parse_input(_input);
-    Some(count_open_sides(points))
+    let (points, _, _) = parse_input(_input);
+    Some(count_open_sides(&points))
 }
 
 pub fn part_two(_input: &str) -> Option<u32> {
-    None
+    let (points, min, max) = parse_input(_input);
+
+    let cube = create_cube(
+        &(min - Point3 { x: 1, y: 1, z: 1 }),
+        &(max + Point3 { x: 2, y: 2, z: 2 }),
+    );
+    let difference = cube.difference(&points).cloned().collect();
+    let outside = find_connected_points(&difference, &(min - Point3 { x: 1, y: 1, z: 1 }));
+    let inside: HashSet<Point3<i32>> = difference.difference(&outside).cloned().collect();
+
+    Some(count_open_sides(&points) - count_open_sides(&inside))
 }
 
 fn main() {
@@ -79,6 +123,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 18);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(58));
     }
 }
